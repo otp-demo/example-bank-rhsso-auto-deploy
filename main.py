9a9b60d242f5db0d0e66c07614399f0b33725a0f
akiyamn <10993186+akiyamn@users.noreply.github.com>
2022-01-31 16:55:44 +1100

    Removed redundant check for realm already existing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import sys
 import json
 
+VERIFY_TLS = False
 BASE_URL = os.environ.get('RHSSO_BASE_URL')
 
 
@@ -13,7 +14,8 @@ def admin_login(username, password):
         "Content-Type": "application/x-www-form-urlencoded"
     }
 
-    response = requests.request("POST", url, data=payload, headers=headers, verify=False)
+    response = requests.request(
+        "POST", url, data=payload, headers=headers, verify=VERIFY_TLS)
     if response.status_code == 200:
         return response.json()["access_token"]
     print(response.status_code, response.text)
@@ -23,28 +25,17 @@ def admin_login(username, password):
 def upload_json(json_string, admin_token):
     url = f"{BASE_URL}/auth/admin/realms"
     payload = json.loads(json_string)
-    print(payload)
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {admin_token}",
     }
 
-    response = requests.request("POST", url, json=payload, headers=headers, verify=False)
+    response = requests.request(
+        "POST", url, json=payload, headers=headers, verify=VERIFY_TLS)
     if response.status_code == 201:
         return True
     print(response.status_code, response.text)
     return False
-
-
-def realm_exists(realm, admin_token):
-
-    url = f"{BASE_URL}/auth/admin/realms/{realm}"
-    payload = ""
-    headers = {
-        "Authorization": f"Bearer {admin_token}"
-    }
-    response = requests.request("GET", url, data=payload, headers=headers, verify=False)
-    return not ("error" in response.json() and response.status_code == 404)
 
 
 def read_json(filename):
@@ -65,16 +56,12 @@ def main():
         print("Unable to login as admin user. Aborting...", file=sys.stderr)
         return 1
 
-    if realm_exists(realm, token):
-        print("Realm already exists. Aborting...", file=sys.stderr)
-        return 2
-
     json = read_json(json_path)
     if not upload_json(json, token):
         print("Unable to create realm. Aborting...", file=sys.stderr)
-        return 3
+        return 2
 
-    print("Sucessfully uploaded realm!")
+    print("Successfully uploaded realm!")
     return 0
 
 

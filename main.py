77b01dff15eadceab46bc294ebe55c3e393acef8
akiyamn <10993186+akiyamn@users.noreply.github.com>
2022-02-11 11:33:02 +1100

    Added option for optional test user creation

--- main.py
+++ main.py
@@ -57,18 +57,42 @@ def read_json(filename):
     with open(filename, "r") as file:
         return file.read()
 
+# Create a single user with a given username and password in the provided realm, given an admin token
+def create_user(new_username, new_password, realm, admin_token):
+    url = f"{BASE_URL}/auth/admin/realms/{realm}/users"
+    payload = {
+        "enabled": "true",
+        "username": new_username,
+        "credentials": [
+            {
+                "type": "password",
+                "value": new_password
+            }
+        ]
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {admin_token}",
+    }
+    response = requests.request("POST", url, json=payload, headers=headers, verify=VERIFY_TLS)
+    if response.status_code == 201:  # Check if new realm created
+        return True
+    print(response.status_code, response.text)
+    return False  # Was unable to create realm
 
-# Main function
+
 def main():
     json_path = "realm-export.json"
 
     # Read in environment variables
-    user = os.environ.get('RHSSO_ADMIN_USER')
-    password = os.environ.get('RHSSO_ADMIN_PASSWORD')
+    admin_username = os.environ.get('RHSSO_ADMIN_USER')
+    admin_password = os.environ.get('RHSSO_ADMIN_PASSWORD')
+    new_username = os.environ.get('RHSSO_TEST_USER')
+    new_password = os.environ.get('RHSSO_TEST_PASSWORD')
 
     # Log in as admin
     print(f"Connecting to {BASE_URL}...")
-    token = admin_login(user, password)
+    token = admin_login(admin_username, admin_password)
 
     # Check if login was successful
     if not token:
@@ -77,13 +101,22 @@ def main():
     print("Logged in as admin user...")
 
     # Upload and apply realm export JSON
-    json = read_json(json_path)
-    if not upload_json(json, token):  # Check errors in upload request
+    json_text = read_json(json_path)
+    if not upload_json(json_text, token):  # Check errors in upload request
         print("Unable to create realm. Aborting...", file=sys.stderr)
         return 2
-
     print("Successfully uploaded realm!")
+
+    # Create the test user if requested
+    if new_username and new_password:
+        realm = json.loads(json_text)["realm"]
+        if not create_user(new_username, new_password, realm, token):
+            print(f"Unable to create the test user \"{new_username}\" in realm {realm}.", file=sys.stderr)
+            return 3
+        print(f"Created user \"{new_username}\" in realm {realm}.")
+
     return 0
 
 
 main()
+
